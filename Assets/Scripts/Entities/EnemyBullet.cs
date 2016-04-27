aa456296fcffd9187965efe0e2d140d956bc878e
Rodrigo Abreu <rodrigo.abreu@ideais.com.br>
2016-04-26 23:31:27 -0400

    changing the structure of the project for also keep track of the graphics projects in photoshop

--- Assets/Scripts/Entities/EnemyBullet.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class EnemyBullet : Bullet {
-
-	void Awake() {
-		target = "Player";
-	}
-}
--- Assets/Scripts/Enums/EffectType.cs
+++
@@ -1,4 +0,0 @@
-﻿public enum EffectType {
-	ScoreUp,
-	Explosion
-}
